66a5881fa510ab0793e48e339a2c3ca18087f2fe
HernanRuizMichel <hagohermichel@gmail.com>
2021-08-26 23:30:30 -0400

    nuevos episodios tokyo revengers part#1

--- Directorio/tokyo-revengers/episodios.js
+++ Directorio/tokyo-revengers/episodios.js
@@ -39,7 +39,7 @@ function ocultarAnterior(){
 ocultarSiguiente();
 
 function ocultarSiguiente(){
-    if (id == 18) {
+    if (id == 20) {
         botonSiguienteO.style.display = "none";
     }
 }
